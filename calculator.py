857d4999c9a423f3c5a7f6df93c3bafa5f09fca9
David Ward <david.a.ward@duke.edu>
2020-01-21 17:59:50 -0500

    Created four arithmatic functions

--- calculator.py
+++ calculator.py
@@ -2,21 +2,42 @@
 
 def add(x, y):
     z = x + y
-    print("{} + {} = {}".format(x, y, z))
     return z
 
 
-x = input("Enter a letter: ")
-print("You entered {}".format(x))
-if x == "a":
-   d = add(56, 73)
-elif x == "s":
-   a = 20
-   b = -3
-   c = a - b
-   print("{} - {} = {}".format(a, b, c))
-else:
-   print("The {} command is not recognized.".format(x))
-   
-print("Done")
+def subtract(x, y):
+    z = x - y
+    return z
+    
 
+def multiply(x, y):
+    z = x * y
+    return z
+
+
+def divide(x, y):
+    z = x / y
+    return z
+    
+
+operator = input("Enter a letter: ")
+print("You entered {}".format(operator))
+a = input("Enter the first number: ")
+b = input("Enter the second number: ")
+if operator == "a":
+   answer = add(int(a), int(b))
+   symbol = '+'
+elif operator == "s":
+   answer = subtract(int(a), int(b))
+   symbol = '-'
+elif operator == "m":
+   answer = multiply(int(a), int(b))
+   symbol = 'x'
+elif operator == "d":
+   answer = divide(int(a), int(b))
+   symbol = '/'
+else:
+   print("The {} command is not recognized.".format(operator))
+   exit()
+
+print("{} {} {} = {}".format(a, symbol, b, answer))
